03aee2c7ed9da0658633214177ddfebe9fd25869
Ekagrata <ekagratasharma@gmail.com>
2019-04-01 00:27:41 +0530

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots=[
+{
+	id: 1,
+	name: 'Leanne Graphm',
+	username: 'Bret',
+	email: 'Sincarebh@april.biz'
+},
+{
+	id: 2,
+	name: 'Rajneesh Sharma',
+	username: 'Rinku',
+	email: 'rasjneshkj@april.biz'
+},
+{
+	id: 3,
+	name: 'Vinita Tanwar',
+	username: 'Neeru',
+	email: 'Vinita_tanwar22@yahoo.in'
+},
+{
+	id: 4,
+	name: 'Ekagrata',
+	username: 'Eku',
+	email: 'ekagratasharma@april.biz'
+},
+{
+	id: 5,
+	name: 'Devangna Maniktala',
+	username: 'dev',
+	email: 'Devangnadev@april.biz'
+},
+{
+	id: 6,
+	name: 'Asryshi Maheshwari',
+	username: 'Nicky',
+	email: 'Asryshi@april.biz'
+},
+{
+	id: 7,
+	name: 'Sarvesh Gupta',
+	username: 'Billu',
+	email: 'rockstar123@gmail.com'
+},
+{
+	id: 8,
+	name: 'Radha',
+	username: 'Rats',
+	email: 'Radha@april.biz'
+},
+{
+	id: 9,
+	name: 'Sophie Glinn',
+	username: 'Sphj',
+	email: 'Sophie@april.biz'
+},
+{
+	id: 10,
+	name: 'Radhe Babu',
+	username: 'Radhe',
+	email: 'RadheMaa@april.biz'
+},
+{
+	id: 11,
+	name: 'Rashanj',
+	username: 'Rajan',
+	email: 'Rajanann@april.biz'
+},
+{
+	id: 12,
+	name: 'Rashika Tanwar',
+	username: 'rash',
+	email: 'Rashika@april.biz'
+}
+]
